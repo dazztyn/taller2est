6f72cd96e7bd358724b9da0b83e9df62b1d75492
PA1IN <144868396+PA1IN@users.noreply.github.com>
2024-05-07 01:59:12 +0000

    doc metodos

--- Sources/Src/Linkedlist.cpp
+++ Sources/Src/Linkedlist.cpp
@@ -29,64 +29,67 @@ void Linkedlist::addValue(int value) // agrega un valor entregado por parametro 
 
 Node* Linkedlist::SearchValue(int value)
 {
-    if(this->head == nullptr)
+    if(this->head == nullptr) // se compara el puntero head para ver si la lista esta vacia
     {
-        return nullptr;
+        return nullptr; // en este caso se retorna null
     }
 
-    Node* aux = this->head;
+    Node* aux = this->head; // se crea una variable "aux" de tipo puntero y se inicializa para que apunte al nodo principal de la lista
 
     if(aux->value == value)
     {
-        return aux;
+        return aux; // en este caso si el valor al que apunta "aux" es el valor buscado se retorna el puntero
     }
-
-    while(aux->next != nullptr)
+    else
     {
-        aux = aux->next;
-        if(aux->value == value)
+        while(aux->next != nullptr) // sino se recorre mientras el puntero siguiente sea distinto de nulo
         {
-            return aux;
+            aux = aux->next;
+            if(aux->value == value) // y se pregunta si el valor al que apunta "aux" es igual al valor buscado
+            {
+                return aux; // en ese caso se devuelve el puntero donde se encuentra
+            }
         }
     }
-    return nullptr;
+    
+    return nullptr; // si no se encuentra se retorna null
 }
 
 void Linkedlist::DeleteValue(int value)
 {
-    Node* valueDel = SearchValue(value);
-    Node* valuePrev = head;
+    Node* valueDel = SearchValue(value); // se inicializa una variable "valorEliminado" de tipo puntero que se le asigna el puntero que retorna la funcion buscarValor con el valor dado por parametro
+    Node* valuePrev = head; //se inicializa otra variable "ValorPrevio" de tipo puntero cuya asignacion es el puntero principal de la lista enlazada
 
-    if(valueDel == nullptr)
+    if(valueDel == nullptr) // si el puntero "valorEliminado" es nulo
     {
-        cout<<"el dato que desea eliminar no existe"<<endl;
+        cout<<"el dato que desea eliminar no existe"<<endl; // se imprime que no se puede borrar, porque no existe
         return;
     }
-    if(valueDel == head)
+    if(valueDel == head) // si el valor a eliminar se encuentra la principio de la lista enlazada
     {
-        head = valueDel->next;
+        head = valueDel->next;  // el puntero principal "head" ahora apunta al siguiente del nodo a eliminar
     }
-    else
+    else // sino
     {
-        while(valuePrev->next != valueDel)
+        while(valuePrev->next != valueDel) 
         {
-            valuePrev = valuePrev->next;
+            valuePrev = valuePrev->next;    // se recorre la lista enlazada mientras el nodo al que apunta "valor previo" sea distinto al del valor a eliminar
         }
-        valuePrev->next = valueDel->next;
+        valuePrev->next = valueDel->next; // luego el puntero "valor previo" apunta hacia el nodo siguiente del que se va a eliminar
     }
 
-    delete valueDel;
+    delete valueDel; //luego procede a borrarse el valor
 }
 
 
 void Linkedlist::print()
 {
-    Node* currentNode = head;
-    while(currentNode != nullptr)
+    Node* currentNode = head; // se crea una variable de tipo puntero "Nodo actual" que va a guardar la direccion de memoria del puntero head 
+    while(currentNode != nullptr) // se recorre la lista enlazada mientras el nodo actual sea distinto de nulo
     {
-        cout<<currentNode->value<<endl;
-        currentNode = currentNode->next;
+        cout<<currentNode->value<<endl; // muestra los datos de la lista
+        currentNode = currentNode->next; // el puntero "nodo actual" ahora apunta al siguiente nodo ( de esta forma vamos cambiando de nodo en la lista)
     }
-    cout<< "nullptr" <<endl;
-    
+    cout<< "nullptr" <<endl; // se imprime nulo cuando finaliza el ciclo while
+
 }
